e86c5971d092cfa6562894b893a0c9a484f9fee8
Joe Schoeler <31747663+joeschoeler13@users.noreply.github.com>
2017-09-30 22:50:35 +0200

    Finally found the reason, why the if statements did not work. I did not proof with '==='. I found the solution here: https://stackoverflow.com/questions/154059/how-do-you-check-for-an-empty-string-in-javascript . Code is ready to publish now.

--- java-index.js
+++ java-index.js
@@ -3,24 +3,22 @@ function contact(){
     
     var chat1 = prompt("Hi! Dow you like to chat? If not, please feel free to write me an e-mail or connect with me on LikedIn. If you like to chat just type in 'YES'.")
     
-    if (chat1 = "YES") {
+    if (chat1 === "YES") {
         var chat2 = prompt("Cool, thanks! My name Joe. What's your name?")
         
-        if (chat2 ="") {
-            alert(bye)
-            return
+        if (chat2 === "") {
+            return alert(bye)
         } else {
-            var chat3 = prompt("Hi" + chat2 + "How are you?")
+            var chat3 = prompt("Hi " + chat2 + "! How are you?")
             
-            if (chat3 = "") {
-
+            if (chat3 === "") {
+                return alert(bye)
             } else {
                 prompt("Good to know! I'm fine. How can I help you?")
-                alert("Oh, I'm sorry I cannot help you in this regard.")
+                alert("Oh, I'm sorry I cannot help you in this regard at the moment. Feel free to ask me via mail. Thanks for chatting. Have a nice day.")
             }
         } 
     } else {
-        alert(bye)
-        return
+        return alert("I see. As I said, please feel free to send me an e-mail or connect with me on LinkedIn.")
     }
 }
