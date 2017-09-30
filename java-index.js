55c5b7ba0c88e950c1a4ab4775398a21f7aae73f
Joe Schoeler <31747663+joeschoeler13@users.noreply.github.com>
2017-09-30 22:28:19 +0200

    Fixed the bug, that all the prompts were not shown. Now they get shown, but do not react to the if statements.

--- java-index.js
+++ java-index.js
@@ -1,30 +1,26 @@
 function contact(){
     var bye = "Oh, I see - you do not want to chat anymore. Have a nice day"
     
-    var chat1 = alert(prompt("Hi! Dow you like to chat? If not, please feel free to write me an e-mail or connect with me on LikedIn. If you like to chat just type in 'YES'."))
+    var chat1 = prompt("Hi! Dow you like to chat? If not, please feel free to write me an e-mail or connect with me on LikedIn. If you like to chat just type in 'YES'.")
     
-    if chat1 = "YES" {
-        var chat2 = alert(prompt("Cool, thanks! My name Joe. What's your name?"))
-
-        if chat2 ="" {
+    if (chat1 = "YES") {
+        var chat2 = prompt("Cool, thanks! My name Joe. What's your name?")
+        
+        if (chat2 ="") {
             alert(bye)
             return
-        }else {
-            var chat3 = alert(prompt("Hi" + chat2 + "How are you?"))
+        } else {
+            var chat3 = prompt("Hi" + chat2 + "How are you?")
+            
+            if (chat3 = "") {
 
-            if chat3 = "" {
-                alert(bye)
-                return
-            }else{
-                var chat4 = alert(prompt("Good to know! I'm fine. How can I help you?"))
+            } else {
+                prompt("Good to know! I'm fine. How can I help you?")
                 alert("Oh, I'm sorry I cannot help you in this regard.")
             }
-        }
+        } 
+    } else {
+        alert(bye)
+        return
     }
-}
-
-function chatme(){
-    var cya = "Oh, I see - you do not want to chat anymore. Have a nice day!"
-
-    var c1 = alert(prompt("Hi! Dow you like to chat? If not, please feel free to write me an e-mail or connect with me on LikedIn. If you like to chat just type in 'YES'."))
 }
