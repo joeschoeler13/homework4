14257d952b755bac654c668678b56aa98cd8925c
Joe Schoeler <31747663+joeschoeler13@users.noreply.github.com>
2017-09-30 20:28:30 +0200

    Put the js-function in its own document and incorporated the source into the script of the html doc. Layout and Text are ready to publish now.

---
+++ java-index.js
@@ -0,0 +1,3 @@
+function contact() {
+    alert("Hi, unfortunately, your message >>" + prompt("Hey, if you want, you can leave me a message!") + "<< could not be sent to me. Please contact me directly via mail (joeschoeler@example.com), via phone ( +1 (250) 123-4567 ) or visit me on LinkedIn (https://www.linkedin.com/in/joeschoeler13/)");
+}
